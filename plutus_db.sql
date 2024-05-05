aa354fb6a8707b4fd632d7d8d2b0fd88215ac900
Chandramouli Iyer <chandramouli.iyer@sjsu.edu>
2024-05-05 03:38:43 -0700

    Made sensible changes to schema and added gitignore.
    Detailed:
    1. Changes to schema primarily include making required attributes to carry a NOT NULL flag to make sure they aren't null (it doesn't make sense that first or last names or credit card numbers be NULL). Also added mobile attribute to Users table, thus reflecting as it was in the Users entity in the backend.
    
    2. Added a gitignore file to ignore binaries and user specific folders. Please add your user specific folders to gitignore as well so it's less cluttered.

--- plutus_db.sql
+++ plutus_db.sql
@@ -1,58 +1,58 @@
-drop database plutus;
 Create database if not exists plutus;
 Use plutus;
 
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    date_of_birth DATE,
-    email VARCHAR(255) UNIQUE
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    mobile INT NOT NULL
 );
 
 CREATE TABLE Banks (
     bank_id INT AUTO_INCREMENT PRIMARY KEY,
-    bank_name VARCHAR(255)
+    bank_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Card_Types (
     card_type_id INT AUTO_INCREMENT PRIMARY KEY,
     bank_id INT,
-    card_type_name VARCHAR(255),
+    card_type_name VARCHAR(255) NOT NULL,
     FOREIGN KEY (bank_id) REFERENCES Banks(bank_id)
 );
 
 CREATE TABLE Cards (
-    card_number VARCHAR(255) PRIMARY KEY,
+    card_number VARCHAR(16) PRIMARY KEY,
     user_id INT,
     card_type_id INT,
-    expiry_date DATE,
+    expiry_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (card_type_id) REFERENCES Card_Types(card_type_id)
 );
 
 CREATE TABLE Product_Categories (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(255)
+    category_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(255),
+    product_name VARCHAR(255) NOT NULL,
     category_id INT,
     FOREIGN KEY (category_id) REFERENCES Product_Categories(category_id)
 );
 
 CREATE TABLE Offer_Types (
     offer_type_id INT AUTO_INCREMENT PRIMARY KEY,
-    offer_type_name VARCHAR(255)
+    offer_type_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Offers (
     offer_id INT AUTO_INCREMENT PRIMARY KEY,
     offer_type_id INT,
     expiry_date DATE,
-    offer_description VARCHAR(255),
+    offer_description VARCHAR(255) NOT NULL,
     FOREIGN KEY (offer_type_id) REFERENCES Offer_Types(offer_type_id)
 );
 
@@ -75,16 +75,16 @@ CREATE TABLE Card_Offers (
 
 CREATE TABLE Companies (
     company_id INT AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR(255),
+    company_name VARCHAR(255) NOT NULL,
     company_url VARCHAR(255)
 );
 
 CREATE TABLE Company_Offers (
     offer_id INT,
-    offer_description VARCHAR(255),
     company_id INT,
-    is_online_offer BOOLEAN,
-    is_in_store_offer BOOLEAN,
+    offer_description VARCHAR(255) NOT NULL,
+    is_online_offer BOOLEAN NOT NULL,
+    is_in_store_offer BOOLEAN NOT NULL,
     PRIMARY KEY (offer_id, company_id),
     FOREIGN KEY (offer_id) REFERENCES Offers(offer_id),
     FOREIGN KEY (company_id) REFERENCES Companies(company_id)
@@ -92,12 +92,12 @@ CREATE TABLE Company_Offers (
 
 CREATE TABLE Stores (
     store_id INT AUTO_INCREMENT PRIMARY KEY,
-    store_name VARCHAR(255),
-    address_line1 VARCHAR(255),
+    store_name VARCHAR(255) NOT NULL,
+    address_line1 VARCHAR(255) NOT NULL,
     address_line2 VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    pin_code VARCHAR(255),
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    zip INT NOT NULL,
     company_id INT,
     FOREIGN KEY (company_id) REFERENCES Companies(company_id)
 );
